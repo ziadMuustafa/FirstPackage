79586e424082089fc0a2219581957765ed11d38d
Ziad Ahmed Mustafa <141859084+ziadMuustafa@users.noreply.github.com>
2024-11-13 18:48:39 +0200

    Add files via upload

---
+++ ExtMethods.cs
@@ -0,0 +1,70 @@
+﻿using NumericSystemConverter.Moldels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericSystemConverter
+{
+    public static class ExtMethods
+    {
+
+        public static void TypeBaseCheck(this string value, string AllowableChars)
+        { // "01"
+
+            foreach (char Char in value)
+            {
+
+                if (!AllowableChars.Contains(Char))
+                {
+
+                    throw new InvalidDataException("Unallowed digits used");
+
+                }
+
+            }
+
+        }
+
+
+        public static string SystemConverterTo<T>(this T source, NumberBase ConvertTo) where T : Base
+        {
+
+            
+
+                NumberBase FromBase;
+
+                switch (source)
+                {
+
+
+
+                    case Binary:
+                        FromBase = NumberBase.Bin; break;
+
+
+                    case Octal:
+                        FromBase = NumberBase.Oct; break;
+
+
+                    case Hexdecimal:
+                        FromBase = NumberBase.Hex; break;
+
+
+                    case Decimal1:
+                        FromBase = NumberBase.Dec; break;
+
+                    default: FromBase = NumberBase.Bin;  break;
+
+
+                }
+
+
+            return Convert.ToString(Convert.ToInt32(source.Value, (int)FromBase) , (int)ConvertTo);
+
+
+        }
+    }
+}
---
+++ NumberBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericSystemConverter
+{
+    public enum NumberBase
+    {
+
+        Bin = 2 ,
+        Oct = 8 , 
+        Dec = 10 ,
+        Hex = 16,
+
+    }
+}
